a466377cf13820e27af2de51570a20b3b931cd85
Vassily Krasnov <mykrasnov@gmail.com>
2020-02-15 18:16:57 +0300

    Receive root data

--- src/components/RootSelect/RootSelect.tsx
+++ src/components/RootSelect/RootSelect.tsx
@@ -1,21 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Select } from 'antd';
 import { useDispatch } from 'react-redux';
 import { baseUrl } from '../../api';
 
 function RootSelect() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
   // const dispatch = useDispatch();
-  const getRoots = (rootUrl: string) => {
-    fetch(rootUrl).then(data => data).then(data => data);
+
+  const getRoots = async (rootUrl: string) => {
+    setLoading(true);
+    const response = await fetch(rootUrl);
+    if (response.ok) {
+      setLoading(false);
+      const json = await response.json();
+      setData(json);
+      console.log(Object.keys(json));
+    } else {
+      setLoading(false);
+      console.log('HTTP Error: ', response.statusText);
+    }
   };
 
   useEffect(() => {
+    console.log('Getting baseURL');
     getRoots(baseUrl);
-  });
+  }, []);
 
   return (
-    <Select>
-      {}
+    <Select loading={loading} style={{width: '150px'}}>
+      {Object.keys(data).map(item => <Select.Option key={item}>{item}</Select.Option>)}
     </Select>
   );
 }
